9591450643e19bc7d04f4c0163e1a20d9ff94586
Habiba Faisal <Habiba.Faisal@kalsoft.com>
2023-09-12 12:32:47 +0500

    Directives concept

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+  products = [
+    {
+      id: 1,
+      name: "Premium Red Sofa",
+      color: "Red",
+      available: "available",
+      image: "../../assets/product.jpg",
+    },
+    {
+      id: 2,
+      name: "Ocean Blue Lounge Chair",
+      color: "Blue",
+      available: "not available",
+      image: "../../assets/product.jpg",
+    },
+    {
+      id: 3,
+      name: "Elegant Pink Dining Table",
+      color: "Pink",
+      available: "available",
+      image: "../../assets/product.jpg",
+    },
+    {
+      id: 4,
+      name: "Sunshine Yellow Hammock",
+      color: "Yellow",
+      available: "not available",
+      image: "../../assets/product.jpg",
+    },
+    {
+      id: 5,
+      name: "Royal Purple Bed Frame",
+      color: "Purple",
+      available: "available",
+      image: "../../assets/product.jpg",
+    },
+];
+
+
+}
