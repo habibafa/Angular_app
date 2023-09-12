3a4b33eb6e3b71ede96e8fd5452619f942324b72
Habiba Faisal <Habiba.Faisal@kalsoft.com>
2023-09-12 14:24:29 +0500

    ngDirectives concepts (styles, classes etc)

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -3,18 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notification',
   // templateUrl: './notification.component.html',
-  template:`<div class="alert alert-success" [hidden]="displayNotification" >
+  template: `
+  
+  <!-- <div class="alert alert-success" [hidden]="displayNotification" > -->
+
+  <div class="alert alert-success" [ngClass]='{fadeOut : displayNotification}'>
+
   This website will diplay something, notifications etc
   <button class='btn' (click)='handleClose()'>Press</button>
   </div>`
   ,
-  styleUrls: ['./notification.component.css']
-  // can use styles: [] instead of styleUrls to add styles here
+  // styleUrls: ['./notification.component.css'],
+  styles: [".fadeOut { visibility: hidden; opacity: 0;transition: visibility 0s 5s, opacity 5s linear;}"]
 })
+// can use styles: [] instead of styleUrls to add styles here
 export class NotificationComponent {
- displayNotification: boolean = false
+  displayNotification: boolean = false
 
- handleClose(){
-  this.displayNotification = true
+  handleClose() {
+    this.displayNotification = true
   }
 }
